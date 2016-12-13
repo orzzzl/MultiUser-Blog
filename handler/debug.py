5509313e1789ea726b98d41b8e3a2c6156052651
Zeleng Zhuang <z@sherlore.com>
2016-12-12 21:10:44 -0500

    Delete debug.py

--- handler/debug.py
+++
@@ -1,9 +0,0 @@
-from base_handler import BaseHandler
-from database.users import User
-from database.blogs import Post
-
-
-class Debug(BaseHandler):
-    def get(self):
-        posts = Post.exe_gql("select * from Post")
-        self.write(posts)
